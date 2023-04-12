2f82282439b903eac2b86e187aa0f6e8ded293f4
MayradelvGonzalez <mayradelvallegonzalez@gmail.com>
2023-04-12 00:32:25 -0300

    creandoApp

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
-console.log('Hello Node World')
+import express from 'express';
+//creo mi app con express
+ const app = express()
+//uso el metodo de express listen para escuchar el puerto
+ app.listen(3000)
+ console.log('Server is listening on PORT 3000')
