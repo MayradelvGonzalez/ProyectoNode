92d599143ad4ba0293db5fcb6c37ef21d0d75704
MayradelvGonzalez <mayradelvallegonzalez@gmail.com>
2023-04-12 01:18:29 -0300

    rutas, paso de datos back a front

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import {dirname, join} from 'path';
 import { fileURLToPath } from 'url';
+import indexRouter from './routes/index';
 
 //creo mi app con express
 const app = express()
@@ -10,9 +11,6 @@ const __dirname = dirname(fileURLToPath(import.meta.url))
 app.set('views', join(__dirname, 'views')) //para saber donde estan las carpetas view
 app.set('view engine', 'ejs') //me permite añadir logica de progr al html
 
-app.get('/', (req, res) => res.render('src/index.ejs')) //renderiza, envia un archivo html que va a 
-//estar ubicado ahi
-
 //uso el metodo de express listen para escuchar el puerto
- app.listen(3000)
- console.log('Server is listening on PORT 3000')
+ app.listen(8080)
+ console.log('Server is listening on PORT 8080')
---
+++ src/routes/index.js
@@ -0,0 +1,11 @@
+import {Router} from 'express';
+
+const router = Router() 
+
+router.get('/', (req, res) => res.render('index', { title: 'First website with Node'})) //renderiza, envia un archivo html que va a 
+//estar ubicado ahi,lo reconoce como un .ejs. {paso un dato de back a front}
+router.get('/about', (req, res) => res.render('about', { title: 'About me'}))
+
+router.get('/contact', (req, res) => res.render('contact', {title: 'Contact me'}))
+
+export default router;
