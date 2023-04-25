45b3823fb7d850c3df82abfc56c453a7816bccc7
MayradelvGonzalez <mayradelvallegonzalez@gmail.com>
2023-04-24 23:11:16 -0300

    estilos y rutas

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import {dirname, join} from 'path';
 import { fileURLToPath } from 'url';
-import indexRouter from './routes/index';
+import indexRouter from './routes/index.js';
 
 //creo mi app con express
 const app = express()
@@ -10,6 +10,8 @@ const __dirname = dirname(fileURLToPath(import.meta.url))
 
 app.set('views', join(__dirname, 'views')) //para saber donde estan las carpetas view
 app.set('view engine', 'ejs') //me permite añadir logica de progr al html
+app.use(indexRouter)
+app.use(express.static(join(__dirname, 'public')))
 
 //uso el metodo de express listen para escuchar el puerto
  app.listen(8080)
