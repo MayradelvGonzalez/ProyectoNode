2ea31eabef63b0a0da3dbb6a5ea41292585c0d24
MayradelvGonzalez <mayradelvallegonzalez@gmail.com>
2023-05-01 00:57:10 -0300

    Estilos

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,11 @@
-import {Router} from 'express';
+import { Router } from 'express';
 
-const router = Router() 
+const router = Router()
 
-router.get('/', (req, res) => res.render('index', { title: 'Node Website'})) //renderiza, envia un archivo html que va a 
+router.get('/', (req, res) => res.render('index', { title: 'Node Website' })) //renderiza, envia un archivo html que va a 
 //estar ubicado ahi,lo reconoce como un .ejs. {paso un dato de back a front}
-router.get('/about', (req, res) => res.render('about', { title: 'About me'}))
+router.get('/about', (req, res) => res.render('about', { title: 'About me' }))
 
-router.get('/contact', (req, res) => res.render('contact', {title: 'Contact me'}))
+router.get('/contact', (req, res) => res.render('contact', { title: 'Contact me' }))
 
 export default router;
